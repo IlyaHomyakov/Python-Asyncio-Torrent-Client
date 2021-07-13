1b5b8fb4ded443cdd1437240d158d0a26709bdd5
i.k.homyakov1@gmail.com <23122001doom>
2021-07-13 16:19:05 +0300

    updated code with some notes

--- peers_requester.py
+++ peers_requester.py
@@ -6,48 +6,49 @@ from torrent_file_reader import *
 import aiohttp
 import asyncio
 
-connections_to_peers_loop = asyncio.get_event_loop()
-handshake = struct.pack(
+connections_to_peers_loop = asyncio.get_event_loop()  # initializing asyncio loop for handling peers connection
+
+# todo check handshake on correctness
+handshake = struct.pack(  # creating data for handshaking a peer
     '>B19s8x20s20s',
     19,
     b'BitTorrent protocol',
     info_hash,
     peer_id.encode()
 )
-
+# handshake_str = '>B19s8x20s20s' + (19).encode() +  litter
 print(handshake)
 
 
-async def connect_to_peer(peer_address):
+async def connect_to_peer(peer_address):  # starting connections to the peer
     try:
-        reader, writer = await asyncio.wait_for(
+        reader, writer = await asyncio.wait_for(  # trying to establish connection
             asyncio.open_connection(peer_address[0], peer_address[1]),
             timeout=10
         )
-
-    except:
+    except:  # if impossible, skip this connection
         return
 
 # todo fix Operation Timed Out while handshaking peer
-    writer.write(handshake)
-    await writer.drain()
-    # peer_handshake = await reader.read(68)
+    writer.write(handshake)  # handshaking a peer
+    await writer.drain()  # buffers the data and arranges for it to be sent out asynchronously.
+    # peer_handshake = await reader.read(68)  doing nothing, stops program normal flow
     # print(peer_handshake)
-    resp = await reader.read()  # Suspends here if there's nothing to be read
+    resp = await reader.read()  # Suspends here if there's nothing to be read, also stops program normal flow
 
     print(resp)
     # print(writer, reader)
 
 
-async def downloads():
+async def downloads():  # for future
     pass
 
 
-async def start_connections():
+async def start_connections():  # function that starts an asynchronous connection with separate peer
     connections = [asyncio.ensure_future(connect_to_peer(j)) for j in peers_addresses]
     # connections = [asyncio.create_task(connect_to_peer(peers_addresses[0]))]
     await asyncio.wait(connections)
     print('The end')
 
 
-connections_to_peers_loop.run_until_complete(start_connections())
+connections_to_peers_loop.run_until_complete(start_connections())  # run connection routine
